e5b4afc118175317c18c471076c937c13edae17f
Nikita Shah <nikita12.shah@gmail.com>
2024-11-20 15:14:26 +0530

    If-Else Demo, Logged-In User.

---
+++ DemoLoggedInUser.js
@@ -0,0 +1,9 @@
+let userLoggedInToday = true;
+
+// let userLoggedInToday = false;
+
+if (userLoggedInToday) {
+    console.log("Thanks for logging in today!");
+} else {
+    console.log("Don't forget to log in today!");
+}
