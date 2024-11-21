3b621c66c93190d42ec4c66f5c86eb58e417b73c
Nikita Shah <nikita12.shah@gmail.com>
2024-11-21 17:43:51 +0530

    Demo for checking equality in JavaScript.

---
+++ DemoCheckingEquality.js
@@ -0,0 +1,14 @@
+// A random number from 1 to 6
+let random = Math.ceil(math.random() * 6);
+
+// Try changing the equality checks below
+// See the results by clicking 'run'
+if (random === 1) {
+    console.log("The random number was 1");
+} else if (random === 2) {
+    console.log("The random number was 2");
+} else if (random === 6) {
+    console.log("The random number was 6");
+} else {
+    console.log("The random number was not 1, 2 or 6. Unlucky!");
+}
