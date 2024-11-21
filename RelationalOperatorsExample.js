3f9089ecc1e3f70487a1eee546951d94e33b958e
Nikita Shah <nikita12.shah@gmail.com>
2024-11-21 20:46:59 +0530

    Example for Relational Operators in JavaScript.

---
+++ RelationalOperatorsExample.js
@@ -0,0 +1,24 @@
+// Greater than - `>`
+7 > 4; // true
+2 > 10; // false
+100 > 100; // false
+
+// Less than - `<`
+7 < 4; // false
+2 < 10; // true
+100 < 100; // false
+
+// Greater than or equal to - `>=`
+7 >= 4; // true
+2 >= 10; // false
+100 >= 100; // true
+
+// Less than or equal to - `<=`
+7 <= 4; // false
+2 <= 10; // true
+100 <= 100; // true
+
+// Not equal - `!==` (opposite of ===)
+7 !== 4; // true
+2 !== 10; // true
+100 !== 100; // false
