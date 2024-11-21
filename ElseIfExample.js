9db46aa2b41fc48b7198f324cb28ccb4f842176b
Nikita Shah <nikita12.shah@gmail.com>
2024-11-21 21:11:37 +0530

    Example for if-else-if in JavaScript.

---
+++ ElseIfExample.js
@@ -0,0 +1,10 @@
+let loginCount = 3;
+if (loginCount > 10) {
+  console.log("That's a lot of logins today, champ! Maybe give it a rest.");
+} 
+else if (loginCount > 0) {
+  console.log("Thanks for logging in!");
+} 
+else {
+  console.log("Don't forget to log in today");
+}
