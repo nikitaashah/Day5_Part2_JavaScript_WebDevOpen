9054b1c4f9db759f0f6252349f38de5efca16c27
Nikita Shah <nikita12.shah@gmail.com>
2024-11-22 14:49:09 +0530

    Summary of the Logical Operators in JavaScript.

---
+++ LogicalOperatorsSummary.js
@@ -0,0 +1,13 @@
+// and (do both sides evaluate to true?)
+true && true; // returns true
+true && false; // returns false
+false && true; // returns false
+
+// or (does either side evaluate to true?)
+true || true; // returns true
+true || false; // returns true
+false || false; // returns false
+
+// not (return the inverse)
+!true; // returns false
+!false; // returns true
